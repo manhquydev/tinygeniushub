ea7cce174a06e9d57498f7cfe9e7d2c65df466bd
Nguyễn Mạnh Quý <118015968+manhquydev@users.noreply.github.com>
2026-02-25 01:40:45 +0700

    feat(analytics): add GA4 + Meta Pixel tracking with client-side event utility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 ﻿import type { Metadata } from "next";
 import { Be_Vietnam_Pro } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const mainFont = Be_Vietnam_Pro({
@@ -31,6 +32,14 @@ export const metadata: Metadata = {
   },
 };
 
+const GA4_ID = process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID;
+const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FB_PIXEL_ID;
+// CSP note: if a Content-Security-Policy header is enforced, add to script-src:
+//   https://www.googletagmanager.com https://www.google-analytics.com
+//   https://connect.facebook.net https://www.facebook.com
+// GA4_ID and FB_PIXEL_ID MUST remain build-time NEXT_PUBLIC_ vars (not runtime/DB-driven)
+// to avoid XSS via string interpolation in dangerouslySetInnerHTML below.
+
 export default async function RootLayout({
   children,
 }: Readonly<{
@@ -40,12 +49,56 @@ export default async function RootLayout({
     <html lang="vi">
       <body className={mainFont.variable}>
         {children}
+
+        {/* Service Worker */}
         <script
           dangerouslySetInnerHTML={{
             __html: `if('serviceWorker'in navigator){window.addEventListener('load',function(){navigator.serviceWorker.register('/sw.js').catch(function(e){console.warn('[SW]',e)})})}`,
           }}
         />
+
+        {/* Google Analytics 4 */}
+        {GA4_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA4_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script
+              id="ga4-init"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','${GA4_ID}',{send_page_view:true});`,
+              }}
+            />
+          </>
+        )}
+
+        {/* Meta Pixel */}
+        {FB_PIXEL_ID && (
+          <>
+            <Script
+              id="fb-pixel"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','${FB_PIXEL_ID}');fbq('track','PageView');`,
+              }}
+            />
+            {/* noscript fallback for attribution in JS-disabled environments */}
+            <noscript>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+                alt=""
+              />
+            </noscript>
+          </>
+        )}
       </body>
     </html>
   );
 }
+
